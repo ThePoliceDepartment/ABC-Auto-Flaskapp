d759d5d7a941c92a87ea1cfd94fd38ac07495089
Kevin Sou <kevinsou.ks@gmail.com>
2019-10-14 00:39:21 -0700

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 #-----------------database--------------
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:rocketTurtle510@localhost/ABCAutoShop'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:*******@localhost/ABCAutoShop'
 db = SQLAlchemy(app)
 
 class User(db.Model):
