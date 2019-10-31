54aa35bcb242a0ec4d9cd2a732f399a720f187e2
PaulFigueroa52 <lazerrock52@yahoo.com>
2019-10-31 11:21:52 -0700

    added search car db

--- static/js/carShowAll.js
+++ static/js/carShowAll.js
@@ -10,11 +10,12 @@ $(document).ready(function(){
         }
         else{
             data = JSON.parse(data);
-            console.log(data[0]);
-            console.log(Object.keys(data).length);
+            console.log(Object.keys(data).length);//print length of db
 			
 			$("#carSearchBtn").click(function() {
-				console.log($('#exampleCarInfo').val());
+				console.log("Searching Car");
+				console.log('Info entered: ' + $('#exampleCarInfo').val());
+				
 				var searchTxt = $('#exampleCarInfo').val();
 				var year = searchTxt.split(' ')[0];
 				var make = searchTxt.split(' ')[1];
@@ -25,14 +26,25 @@ $(document).ready(function(){
 				
 				for (i = 0; i < Object.keys(data).length; i++){
 					if(year == data[i].year && make == data[i].make && model == data[i].model){
-						console.log('found car');
 						$('.results').append(searchTxt + ' is in database and we have '+data[i].quantity);
 						found = 1;
+						console.log('CAR FOUND');
 					}
 				}
 				if(found == 0){
-					$('.results').append(searchTxt + ' is not database');
+					$('.results').append(searchTxt + ' is not in database');
+					console.log('NOT FOUND');
+				}
+			});
+			$("#availCarsBtn").click(function() {
+				console.log("Showing all cars in inventory");
+				
+				$('.results').empty();
+				for (i = 0; i < Object.keys(data).length; i++){
+					if(0 < data[i].quantity){
+						$('.results').append((data[i].year +' '+ data[i].make + ' ' + data[i].model + '<br />'));
 
+					}
 				}
 			});
         }
